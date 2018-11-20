369a98c246b0702416896b8d413005bcc9386ce3
Paul Zierep <paul.zierep@pharmazie.uni-freiburg.de>
2018-11-20 13:25:52 +0100

    Final corrections, only windows standalone missing

--- ssrviz/setup_pypi.py
+++ ssrviz/setup_pypi.py
@@ -46,7 +46,7 @@ with open("README.md", "r") as fh:
 
 setup(
    name = "ssrviz",
-    version = "0.1.2.12",
+    version = "1.0.0",
     description = "Protein subfamily specific residue (SSR) detection and visualization toolbox",
     #options = {"build_exe": build_exe_options},
     url = 'http://phabi.de/',
